4447e7e47f400d38d0b12e5875e66c81c3390807
Tyuiu.KhuzinaDR@outlook.com <dianadirtt@bk.ru>
2024-09-26 00:30:48 +0500

    Task7

---
+++ Tyuiu.KhuzinaDR.Sprint0.Task7.V0.Lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KhuzinaDR.Sprint0.Task7.V0.Lib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KhuzinaDR.Sprint0.Task7.V0.Lib")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a76ae8f-4b9b-4bcd-9bad-129b46b5f5cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
